65acb325649adf198fadfb0e9af934a5caeac2dc
steveo1485 <stevennugent@gmail.com>
2014-08-16 20:24:15 -0700

    Display post on post/show. Add feature test for viewing specific post

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -14,4 +14,16 @@ describe "Posts" do
     page.should_not have_content post_two.title
     page.should_not have_content post_two.content
   end
+
+  context "viewing post" do
+    before :each do
+      click_link "Read More"
+    end
+
+    it "should display specific post content" do
+      current_path.should == post_path(post_one.id)
+      page.should have_content post_one.title
+      page.should have_content post_one.content
+    end
+  end
 end
