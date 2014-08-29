1a33f452bffe08bef82af0151b226197b39c90e1
steveo1485 <stevennugent@gmail.com>
2014-08-29 15:37:58 -0700

    Add tests on Post model for set_activated_at after_save

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,25 @@ describe Post do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:content) }
 
+  context "#set_activated_at after_save" do
+    it "should set activated_at after save if active is true" do
+      post = FactoryGirl.build(:post, active: true)
+      post.save
+      expect(post.activated_at).to_not be_nil
+    end
+
+    it "should remove existing activated_at after save if active is false" do
+      post = FactoryGirl.create(:post, active: true)
+      post.active = false
+      post.save
+      expect(post.activated_at).to be_nil
+    end
+
+    it "should not set activated_at if active is false" do
+      post = FactoryGirl.build(:post, active: false)
+      post.save
+      expect(post.activated_at).to be_nil
+    end
+  end
+
 end
