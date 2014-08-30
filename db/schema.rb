68783cf2350e04a9763756035ff22021ddeaae7e
steveo1485 <stevennugent@gmail.com>
2014-08-30 10:31:25 -0700

    Add first_name, last_name and username columns to Users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140830165250) do
+ActiveRecord::Schema.define(version: 20140830173001) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 20140830165250) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
