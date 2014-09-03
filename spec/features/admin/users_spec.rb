40c396c85a4885c947d7386a811a10d265ce0012
steveo1485 <stevennugent@gmail.com>
2014-09-03 09:40:58 -0700

    Build out Edit view for Admin::Users Add Manage/Edit links on Admin::Users Index Add feature test for updating user

--- spec/features/admin/users_spec.rb
+++ spec/features/admin/users_spec.rb
@@ -11,6 +11,8 @@ describe 'Managing Users' do
 
   context "Index" do
     it "should display User info on index" do
+      current_path.should eq(admin_users_path)
+
       within "#user-#{user.id}" do
         page.should have_content user.id
         page.should have_content user.name
@@ -26,4 +28,29 @@ describe 'Managing Users' do
     end
   end
 
+  context "Editing" do
+    before :each do
+      within "#user-#{user.id}" do
+        click_link "Manage User"
+        click_link "Edit User"
+      end
+    end
+
+    it "should allow editing of user with valid attributes" do
+      current_path.should eq(edit_admin_user_path(user.id))
+      fill_in "user_first_name", with: "Updated First"
+      click_button "Update User"
+      current_path.should eq(admin_users_path)
+      within "#user-#{user.id}" do
+        page.should have_content "Updated First"
+      end
+    end
+
+    it "should display errors when using invalid attributes" do
+      fill_in "user_first_name", with: " "
+      click_button "Update User"
+      page.should have_content "Oops! Something went wrong..."
+      page.should have_content "First name can't be blank."
+    end
+  end
 end
